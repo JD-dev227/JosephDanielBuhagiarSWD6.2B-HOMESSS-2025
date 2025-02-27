06093f14af1996cd62ab77a7725da23c6c0a4bca
JD-dev227 <jdmc.buhagiar@gmail.com>
2025-02-27 15:02:36 +0000

    fixed issue with blade views where page did not show up and added models

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CollegeController;
 use App\Http\Controllers\StudentController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
