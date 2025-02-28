1dfbcdcaccaec05816cf023a53acdafb34e3d6c4
JD-dev227 <jdmc.buhagiar@gmail.com>
2025-02-28 21:06:24 +0000

    styled alert partial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,15 @@
 @section('title', 'Home')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 <div class="text-center">
     <h1>Welcome to College Management System</h1>
     <p class="lead">Easily manage students and colleges with our system.</p>
 
+
+
     <button type="button" class="btn btn-outline-primary" onclick="window.location='{{ route('students.index') }}'">View Students</button>
     <button type="button" class="btn btn-outline-dark" onclick="window.location='{{ route('colleges.index') }}'">View Colleges</button>
 </div>
+
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'College Management')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/lux/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    
 </head>
 <body>
 
@@ -17,5 +19,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
 </body>
+
 </html>
